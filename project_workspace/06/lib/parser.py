62b87377f980514a14b4c718c94665d19d39222b
liam <liam@84143535-b54b-0410-b13e-a60e24777d11>
2010-06-18 06:49:27 +0000

    create an output stream
    git-svn-id: http://liamchristopher.com/svn/tecs@1267 84143535-b54b-0410-b13e-a60e24777d11

--- project_workspace/06/lib/parser.py
+++ project_workspace/06/lib/parser.py
@@ -1,11 +1,24 @@
-
+from os import path,mkdir
 class Parser:
     def __init__(self,input_file):
         print("initializing parser")
-        self.__parse_file(input_file)
+        print("loading input")
+        input = self.__parse_file(input_file)
+        print("creating output")
+        output = self.__create_output(input_file)
+        
 
     def __parse_file(self, input_file):
         print("opening: " + str(input_file))
         f = open(input_file)
-        lines = f.readlines()
+        return f.readlines()
 
+    #returns a writeable output handle to "build"
+    #todo: y'know.
+    def __create_output(self, input_file_name):
+        build = "build"
+        base = path.splitext(path.basename(input_file_name))[0]
+        print "base: " + base
+        if(not(path.exists(build))):
+            mkdir(build)
+        return open(build+"/"+base+".hack","w")
