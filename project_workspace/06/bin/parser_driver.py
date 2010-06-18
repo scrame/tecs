34248f7db4a4069f04fa7de93c20e246a8c5108b
liam <liam@84143535-b54b-0410-b13e-a60e24777d11>
2010-06-18 06:49:08 +0000

    basic parser shell.
    git-svn-id: http://liamchristopher.com/svn/tecs@1264 84143535-b54b-0410-b13e-a60e24777d11

---
+++ project_workspace/06/bin/parser_driver.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from parser import Parser
+from sys import argv
+
+#only care about the first argument.
+if(None == argv[1]):
+    print("No input file specified.")
+    exit(255)
+
+file = argv[1]
+
+parser = Parser(file)
+
---
+++ project_workspace/06/lib/parser.py
@@ -0,0 +1,7 @@
+
+class Parser:
+    def __init__(self, input_file):
+        print("initializing parser")
+        print("opening: " + str(input_file))
+        f = open(input_file)
+        
